dffd87abb1ba9abdb2290ffe45eecee652f5f8a5
Josh Sommer <JoshDSommer@gmail.com>
2018-06-16 21:50:03 -0400

    switching project over to ng-packagr

---
+++ app/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptCommonModule } from "nativescript-angular/common";
+
+import { HomeRoutingModule } from "./home-routing.module";
+import { HomeComponent } from "./home.component";
+import { SlidesModule } from "~/lib/src";
+
+
+
+@NgModule({
+    imports: [
+        NativeScriptCommonModule,
+        HomeRoutingModule,
+        SlidesModule
+    ],
+    declarations: [
+        HomeComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class HomeModule { }
