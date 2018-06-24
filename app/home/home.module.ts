60aa3a8ffe7df182be4af76bfab96fd19a3ea8ea
Josh Sommer <JoshDSommer@gmail.com>
2018-06-24 00:45:59 -0400

    moving lib out of app folder

--- app/home/home.module.ts
+++ app/home/home.module.ts
@@ -3,21 +3,11 @@ import { NativeScriptCommonModule } from "nativescript-angular/common";
 
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
-import { SlidesModule } from "~/lib/src";
-
-
+import { SlidesModule } from "nativescript-ngx-slides";
 
 @NgModule({
-    imports: [
-        NativeScriptCommonModule,
-        HomeRoutingModule,
-        SlidesModule
-    ],
-    declarations: [
-        HomeComponent
-    ],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  imports: [NativeScriptCommonModule, HomeRoutingModule, SlidesModule],
+  declarations: [HomeComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class HomeModule { }
+export class HomeModule {}
